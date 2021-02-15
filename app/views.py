c2a91d1dbdb9198f1b9a08b91bf31e9443f83ae0
Kyle Henry <kylehenry003@gmail.com>
2021-02-14 22:14:04 -0400

    Profile page added

--- app/views.py
+++ app/views.py
@@ -7,7 +7,7 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+from datetime import date
 
 ###
 # Routing for your application.
@@ -24,7 +24,15 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+@app.route('/profile')
+def profile():
+    """Render the website's profile page."""
+    return render_template('profile.html')
 
+def format_date_joined(date_joined):
+    now= datetime.datetime,now()
+    date_joined= (date_joined.strftime("%Y-%m-$d"))
+    print ("Joined"+ date_joined)
 ###
 # The functions below should be applicable to all Flask apps.
 ###
